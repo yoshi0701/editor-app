3a770eeaa9b2ac12c5baa69bd2b124279b3fbd2c
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-09-18 22:07:35 +0900

    init project

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
+import CellList from '../../../workspace2/udemy/reactAndTS/jsbook/src/components/cell-list';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <div>
+        <CellList />
+      </div>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
